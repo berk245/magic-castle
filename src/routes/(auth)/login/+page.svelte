6aa5d94bae78a79ccfcfa1e03b43cdd934b453d4
berk.ozzambak <berkozzambak@gmail.com>
2023-10-15 12:23:18 +0200

    Fix form submit bug

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -11,8 +11,8 @@
 </script>
 
 <h2>Login</h2>
-<form>
-	<div class="input-group" on:submit|preventDefault={handleSubmit}>
+<form on:submit|preventDefault={handleSubmit}>
+	<div class="input-group">
 		<input type="text" placeholder="Username" bind:value={username} />
 		<input type="password" placeholder="Password" bind:value={password} />
 		<button type="submit">Login</button>
