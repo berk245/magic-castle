e837ffef6e729547775d0d61d8b7450ba262841e
berk.ozzambak <berkozzambak@gmail.com>
2023-10-15 12:28:26 +0200

    Add initial access token check on application load

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
+<script>
+	import { onMount } from "svelte";
+    import {goto} from '$app/navigation'
+
+    onMount(() => {
+        // Check for access token in localStorage. Redirect to login page if no token is saved
+        const token = localStorage.getItem('access_token');
+        if(!token) goto('/login')
+    })
+</script>
+
 <h1>Magic Castle App</h1>
