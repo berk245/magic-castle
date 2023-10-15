b6f290e503a98410ac7a7e8f5cb23069c92eea18
berk.ozzambak <berkozzambak@gmail.com>
2023-10-15 11:32:02 +0200

    Create login route with a basic form

---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    let username = '';
+    let password = '';
+</script>
+
+<h2>Login</h2>
+<form>
+    <div class="input-group">
+      <input
+        type="text"
+        placeholder="Username"
+        bind:value={username}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        bind:value={password}
+      />
+      <button type="submit">Login</button>
+    </div>
+  </form>
