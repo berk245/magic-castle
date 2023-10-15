1bde002e3960ce811efe73e935359673966bf62d
berk.ozzambak <berkozzambak@gmail.com>
2023-10-15 11:32:56 +0200

    Add a submit handler function for login

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,21 +1,16 @@
 <script>
-    let username = '';
-    let password = '';
+	let username = '';
+	let password = '';
+	const handleSubmit = () => {
+		console.log(username, password);
+	};
 </script>
 
 <h2>Login</h2>
 <form>
-    <div class="input-group">
-      <input
-        type="text"
-        placeholder="Username"
-        bind:value={username}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        bind:value={password}
-      />
-      <button type="submit">Login</button>
-    </div>
-  </form>
+	<div class="input-group" on:submit|preventDefault={handleSubmit}>
+		<input type="text" placeholder="Username" bind:value={username} />
+		<input type="password" placeholder="Password" bind:value={password} />
+		<button type="submit">Login</button>
+	</div>
+</form>
