412066ce247009295a4dccbe7b4437f9a3e5d9e1
berk.ozzambak <berkozzambak@gmail.com>
2023-10-20 00:09:09 +0200

    Handle redirection on load based on auth state

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,30 @@
 <script>
 	import { auth } from '../lib/firebase/firebase';
 	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
 
 	const noAuthRoutes = ['/', 'login', 'signup'];
 
 	onMount(() => {
 		const unsubscribe = auth.onAuthStateChanged(async (user) => {
 			console.log(user);
+			const currentPath = window.location.pathname;
+			console.log(currentPath);
+			if (!user && !noAuthRoutes.includes(currentPath)) {
+				// Unauthenticated user wants to go to a protected route
+				goto('/login');
+				return;
+			}
+			if (!user && noAuthRoutes.includes(currentPath)) {
+				// Unauthenticated user wants to go to a public route
+				goto(currentPath);
+				return;
+			}
+			if (user) {
+                // Authenticated user
+				goto(currentPath);
+				return;
+			}
 		});
 	});
 </script>
