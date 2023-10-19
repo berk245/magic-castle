46104415d46b43c9caa07ca810a8d87f13997975
berk.ozzambak <berkozzambak@gmail.com>
2023-10-19 23:13:18 +0200

    Change state variable name to email

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,12 +1,12 @@
 <script>
-	let username = '';
+	let email = '';
 	let password = '';
 </script>
 
 <h2>Login</h2>
 <form on:submit|preventDefault={handleSubmit}>
 	<div class="input-group">
-		<input type="text" placeholder="Username" bind:value={username} />
+		<input type="text" placeholder="Email" bind:value={email} />
 		<input type="password" placeholder="Password" bind:value={password} />
 		<button type="submit">Login</button>
 	</div>
