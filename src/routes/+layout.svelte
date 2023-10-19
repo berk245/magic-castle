8604b33446318bb2161d117aed77644e62a2b195
berk.ozzambak <berkozzambak@gmail.com>
2023-10-20 00:41:12 +0200

    Change scope of currentPath state variable

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,14 @@
 	import { auth } from '../lib/firebase/firebase';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
-    import {authHandlers} from '$lib/handlers/auth'
+	import { authHandlers } from '$lib/handlers/auth';
 
 	const noAuthRoutes = ['/', 'login', '/signup'];
-
+	let currentPath;
 	onMount(() => {
 		const unsubscribe = auth.onAuthStateChanged(async (user) => {
 			console.log(user);
-			const currentPath = window.location.pathname;
+			currentPath = window.location.pathname;
 			console.log(currentPath);
 			if (!user && !noAuthRoutes.includes(currentPath)) {
 				// Unauthenticated user wants to go to a protected route
@@ -22,20 +22,19 @@
 				return;
 			}
 			if (user) {
-                // Authenticated user
+				// Authenticated user
 				goto(currentPath);
 				return;
 			}
 		});
 	});
 
-    const handleSignout = async() => {
-        await authHandlers.signout()
-    }
-    
+	const handleSignout = async () => {
+		await authHandlers.signout();
+	};
 </script>
 
 <div class="page-header">
-    <button on:click={handleSignout}>Sign Out</button>
+	<button on:click={handleSignout}>Sign Out</button>
 </div>
 <slot />
