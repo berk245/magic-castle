b1b5d344d4811bb1ef9df63ec43a63802d2b2c6e
berk.ozzambak <berkozzambak@gmail.com>
2023-10-19 23:11:45 +0200

    Fix import directory

--- src/lib/handlers/auth.js
+++ src/lib/handlers/auth.js
@@ -1,5 +1,5 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { auth } from "../lib/firebase/firebase";
+import { auth } from "$lib/firebase/firebase";
 
 export const authHandlers = {
     signup: async({email, password}) => {
