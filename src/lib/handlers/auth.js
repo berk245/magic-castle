c7313de301b505cb7913952853bd5405a2d9a23e
berk.ozzambak <berkozzambak@gmail.com>
2023-10-19 23:00:40 +0200

    Add login function

--- src/lib/handlers/auth.js
+++ src/lib/handlers/auth.js
@@ -1,8 +1,11 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../lib/firebase/firebase";
 
 export const authHandlers = {
     signup: async({email, password}) => {
         await createUserWithEmailAndPassword(auth, email, password)
     },
+    login: async({email, password}) => {
+        await signInWithEmailAndPassword(auth, email, password)
+    }
 }
