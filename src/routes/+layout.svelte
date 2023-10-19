69689c59a37562eb55e93430ddbeaf29a9fa873d
berk.ozzambak <berkozzambak@gmail.com>
2023-10-20 01:10:34 +0200

    Remove header from the main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
 	import { auth } from '../lib/firebase/firebase';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
-	import { authHandlers } from '$lib/handlers/auth';
 
 	const noAuthRoutes = ['/', '/login', '/signup'];
 	let currentPath;
@@ -26,13 +25,6 @@
 			}
 		});
 	});
-
-	const handleSignout = async () => {
-		await authHandlers.signout();
-	};
 </script>
 
-<div class="page-header">
-	<button on:click={handleSignout}>Sign Out</button>
-</div>
 <slot />
