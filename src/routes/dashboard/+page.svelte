f56345bdf7d04098aa4f1de7e6239ec336898d21
niccolo.mg <niccolo.manych@posteo.de>
2023-10-21 16:36:04 +0200

    Render dashboard if user is defined
    Previously, we were only checking for loading state. That resulted
    in momentary renders of dashboard before auto redirect to /login.
    With this check, the render does not happen anymore and the jumpy
    behavior is fixed.

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -2,7 +2,7 @@
 	import { authStore } from '$lib/store/store';
 </script>
 
-{#if !$authStore.loading}
+{#if (!$authStore.loading && $authStore.user)}
 	<h1>User dashboard</h1>
     <p>Welcome, user email: {$authStore.user}</p>
 {/if}
