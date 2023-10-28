10d595338e8ac060742e293f90a69e88710b3d5f
Guille <gfgorostidi@protonmail.com>
2023-10-28 14:01:51 +0200

    Restore Sigout button andlayout page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,30 @@
 <script>
 	import '../app.postcss';
+	import { auth } from '../lib/firebase/firebase';
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	const noAuthRoutes = ['/', '/login', '/signup'];
+	let currentPath;
+	onMount(() => {
+		const unsubscribe = auth.onAuthStateChanged(async (user) => {
+			currentPath = window.location.pathname;
+			if (!user && !noAuthRoutes.includes(currentPath)) {
+				// Unauthenticated user wants to go to a protected route
+				goto('/login');
+				return;
+			}
+			if (!user && noAuthRoutes.includes(currentPath)) {
+				// Unauthenticated user wants to go to a public route
+				goto(currentPath);
+				return;
+			}
+			if (user) {
+				// Authenticated user
+				goto(currentPath);
+				return;
+			}
+		});
+	});
 </script>
 
 <slot />
