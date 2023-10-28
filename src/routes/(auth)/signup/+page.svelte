73febf9974de33336fa8904ec2e211620df0800b
Guille <gfgorostidi@protonmail.com>
2023-10-28 14:02:27 +0200

    Apply format

--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -20,39 +20,39 @@
 		class="bg-yellow-400 dark:bg-gray-800 h-screen overflow-hidden flex items-center justify-center"
 	>
 		<div class="bg-white lg:w-6/12 md:7/12 w-8/12 shadow-3xl rounded-xl p-12 md:p-24">
-				<div class="flex items-center text-lg mb-6 md:mb-8">
-					<input
-						type="text"
-						id="username"
-						class="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full"
-						placeholder="Email"
-						bind:value={email}
-					/>
-				</div>
-				<div class="flex items-center text-lg mb-6 md:mb-8">
-					<input
-						type="password"
-						id="password"
-						class="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full"
-						placeholder="Password"
-						bind:value={password}
-					/>
-				</div>
-				<div class="flex items-center text-lg mb-6 md:mb-8">
-					<input
-						type="password"
-						id="password"
-						class="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full"
-						placeholder="Confirm password"
-						bind:value={confirmPassword}
-					/>
-				</div>
-				<button
-					class="bg-gradient-to-b from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white uppercase w-full rounded"
-					type="submit"
-				>
-					Sign Up
-				</button>
+			<div class="flex items-center text-lg mb-6 md:mb-8">
+				<input
+					type="text"
+					id="username"
+					class="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full"
+					placeholder="Email"
+					bind:value={email}
+				/>
+			</div>
+			<div class="flex items-center text-lg mb-6 md:mb-8">
+				<input
+					type="password"
+					id="password"
+					class="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full"
+					placeholder="Password"
+					bind:value={password}
+				/>
+			</div>
+			<div class="flex items-center text-lg mb-6 md:mb-8">
+				<input
+					type="password"
+					id="password"
+					class="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full"
+					placeholder="Confirm password"
+					bind:value={confirmPassword}
+				/>
+			</div>
+			<button
+				class="bg-gradient-to-b from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white uppercase w-full rounded"
+				type="submit"
+			>
+				Sign Up
+			</button>
 		</div>
 	</div>
 </form>
