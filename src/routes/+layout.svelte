80867754beb5772fc049bffe0dd9e23d2c3e0b8c
Guille <gfgorostidi@protonmail.com>
2023-10-30 12:04:16 +0100

    Restructures layout and signout properly

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,28 @@
 <script>
 	import '../app.postcss';
 	import { auth } from '../lib/firebase/firebase';
+	import { authStore } from '$lib/store/store';
+	import { authHandlers } from '$lib/handlers/auth';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 	const noAuthRoutes = ['/', '/login', '/signup'];
 	let currentPath;
+
+	const updateAuthStore = (authStore, user) => {
+		authStore.update((curr) => {
+			return {
+				...curr,
+				user: user?.email || null,
+				loading: false
+			};
+		});
+	};
+
 	onMount(() => {
 		const unsubscribe = auth.onAuthStateChanged(async (user) => {
+			//Update the auth store with new user data
+			updateAuthStore(authStore, user);
+
 			currentPath = window.location.pathname;
 			if (!user && !noAuthRoutes.includes(currentPath)) {
 				// Unauthenticated user wants to go to a protected route
@@ -27,4 +43,9 @@
 	});
 </script>
 
+{#if !$authStore.loading && $authStore.user}
+	<div class="header">
+		<button on:click={authHandlers.signout}>Signout</button>
+	</div>
+{/if}
 <slot />
--- src/routes/dashboard/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import { authHandlers } from '$lib/handlers/auth';
-</script>
-
-<div class="header">
-	<button on:click={authHandlers.signout}>Signout</button>
-</div>
-<slot />
