f28ca0c74e951dfe61bf17032522906610518a42
niccolo.mg <niccolo.manych@posteo.de>
2023-10-30 23:50:41 +0300

    Add reset password component

---
+++ src/routes/(auth)/reset-password/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+	import { authHandlers } from '$lib/handlers/auth';
+	import { goto } from '$app/navigation';
+	let email = '';
+
+	const handleSubmit = async () => {
+		console.log('To be implemented');
+	};
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+	<div
+		class="bg-yellow-400 dark:bg-gray-800 h-screen overflow-hidden flex items-center justify-center"
+	>
+		<div class="bg-white lg:w-6/12 md:7/12 w-8/12 shadow-3xl rounded-xl p-12 md:p-24">
+			<div class="flex items-center text-lg mb-6 md:mb-8">
+				<input
+					type="text"
+					id="username"
+					class="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full"
+					placeholder="Email"
+					bind:value={email}
+				/>
+			</div>
+			<button
+				class="bg-gradient-to-b from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white uppercase w-full rounded"
+				type="submit"
+			>
+				Reset Password
+			</button>
+			<div class="font-small mt-4">
+				<a
+					href="/login"
+					class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+					>Back to login page</a
+				>
+			</div>
+		</div>
+	</div>
+</form>
