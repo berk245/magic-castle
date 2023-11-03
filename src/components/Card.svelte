6e2a606f8ac61f2eb7fe685e5de1b43ca1d8b6e1
Guille <gfgorostidi@protonmail.com>
2023-11-03 18:23:41 +0100

    Creates Card component and basic dashboard structure

---
+++ src/components/Card.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let trick;
+</script>
+
+<div class="bg-slate-400 text-white rounded-md p-2">
+	<p class="text-lg font-bold truncate">{trick.title}</p>
+	<p>{trick.standing ? 'Standing' : 'Sitting'}</p>
+	<p>{trick.minutes} mins</p>
+	<p class="capitalize truncate">{trick.mix} deck</p>
+</div>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,8 +1,26 @@
 <script>
 	import { authStore } from '$lib/store/store';
+	import Card from '$components/Card.svelte';
+
+	const tricks = Array.from({ length: 23 }).map((_, i) => {
+		const id = i + 1;
+		return {
+			id,
+			title: `Trick ${id}`,
+			standing: id % 2 === 0,
+			minutes: Math.floor(Math.random() * 19) + 1,
+			mix: id % 3 === 0 ? 'regular' : 'tricked'
+		};
+	});
 </script>
 
 {#if !$authStore.loading && $authStore.user}
 	<h1>User dashboard</h1>
 	<p>Welcome, user email: {$authStore.user}</p>
 {/if}
+
+<section class="bg-slate-800 gap-4 p-4 grid gap-4 grid-cols-6">
+	{#each tricks as trick}
+		<Card {trick} />
+	{/each}
+</section>
