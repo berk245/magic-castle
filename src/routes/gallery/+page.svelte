594e61f61ccfd6603af3aceafc2620b07e47f255
berk.ozzambak <berkozzambak@gmail.com>
2023-11-15 19:52:20 +0100

    Render accessed tricks in gallery

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Card from '$components/Card.svelte';
 	import { authStore } from '$lib/store/store';
-	
+
 	const tricks = Array.from({ length: 20 }).map((_, i) => {
 		const id = i + 1;
 		return {
@@ -15,7 +15,7 @@
 </script>
 
 <section class="grid p-4 gap-4 lg:grid-cols-6 md:grid-cols-4 grid-cols-2">
-	{#each tricks as trick}
+	{#each $authStore.tricks as trick}
 		<Card {trick} />
 	{/each}
 </section>
