a1578487f52caa1f622d767925103622c0d60863
berk.ozzambak <berkozzambak@gmail.com>
2023-11-15 19:52:52 +0100

    Remove hardcoded tricks

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,17 +1,6 @@
 <script>
 	import Card from '$components/Card.svelte';
 	import { authStore } from '$lib/store/store';
-
-	const tricks = Array.from({ length: 20 }).map((_, i) => {
-		const id = i + 1;
-		return {
-			id,
-			title: `Trick ${id}`,
-			standing: id % 2 === 0,
-			minutes: Math.floor(Math.random() * 19) + 1,
-			mix: id % 3 === 0 ? 'regular' : 'tricked'
-		};
-	});
 </script>
 
 <section class="grid p-4 gap-4 lg:grid-cols-6 md:grid-cols-4 grid-cols-2">
