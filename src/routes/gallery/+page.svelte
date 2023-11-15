fe5167bfb403f4df917f2b15d6197fea2f6bf453
berk.ozzambak <berkozzambak@gmail.com>
2023-11-15 19:54:24 +0100

    Add loading feedback in gallery while accessing firestorm data

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -3,8 +3,12 @@
 	import { authStore } from '$lib/store/store';
 </script>
 
-<section class="grid p-4 gap-4 lg:grid-cols-6 md:grid-cols-4 grid-cols-2">
-	{#each $authStore.tricks as trick}
-		<Card {trick} />
-	{/each}
-</section>
+{#if $authStore.loading}
+	<p>Loading</p>
+{:else}
+	<section class="grid p-4 gap-4 lg:grid-cols-6 md:grid-cols-4 grid-cols-2">
+		{#each $authStore.tricks as trick}
+			<Card {trick} />
+		{/each}
+	</section>
+{/if}
