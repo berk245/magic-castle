68fee3b15bfc64a1920dcd6340aa5ce9306d2957
berk.ozzambak <berkozzambak@gmail.com>
2023-11-15 20:27:11 +0100

    Render spinner in gallery view while fetching data

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,10 +1,11 @@
 <script>
 	import Card from '$components/Card.svelte';
 	import { authStore } from '$lib/store/store';
+	import Spinner from '$components/Spinner.svelte'
 </script>
 
 {#if $authStore.loading}
-	<p>Loading</p>
+ <Spinner/>
 {:else}
 	<section class="grid p-4 gap-4 lg:grid-cols-6 md:grid-cols-4 grid-cols-2">
 		{#each $authStore.tricks as trick}
