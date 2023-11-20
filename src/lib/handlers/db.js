ecb3ddff71317a3d77ea506fa9ff5886c50f9817
niccolo.mg <niccolo.manych@posteo.de>
2023-11-21 00:00:43 +0100

    Add delete trick function

--- src/lib/handlers/db.js
+++ src/lib/handlers/db.js
@@ -1,5 +1,11 @@
 import { db } from '$lib/firebase/firebase';
 import { collection, doc, getDoc, getDocs, setDoc } from 'firebase/firestore';
+import { authStore } from '../store/store';
+
+let user;
+authStore.subscribe((curr) => {
+	user = curr?.user;
+});
 
 export const getUserTricks = async (user) => {
 	if (!user) return [];
@@ -32,4 +38,18 @@ const createUserDocument = async (user) => {
 	);
 	console.log('User created');
 	return;
+};
+
+
+export const deleteTrick = async (trickId) => {
+	try {
+		const userRef = doc(db, 'users', user.uid);
+		const tricksCollection = collection(userRef, 'tricks');
+		const trickDoc = doc(tricksCollection, trickId);
+		await deleteDoc(trickDoc);
+	} catch (err) {
+		console.log(err);
+	} finally {
+		await getUserTricks(user);
+	}
 };
